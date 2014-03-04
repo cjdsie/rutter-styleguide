87434392b1a243bd0d25fd3769abbe72487ff4e2
cjdsie <cjdsie@gmail.com>
2014-03-04 07:11:13 -0330

    v0.0.1 - first commit

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+/*jslint node: true */
+'use strict';
+
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {                                 // Task
+			dist: {                             // Target
+				files: {                        // Dictionary of files
+					'css/pattern.css': 'css/pattern.scss'     // 'destination': 'source'
+					}
+			},
+			dev: {                              // Another target
+				options: {
+					style: 'compacted',                      // Dictionary of render options
+					includePaths: ['stylesheets', 'admin/','patterns/', 'optional/']
+				}
+			}
+		},
+		uglify: {
+			build: {
+				src: 'js/pattern.js',
+				dest: 'js/pattern.min.js'
+			}
+		},
+		cssmin: {
+			minify: {
+				expand: true,
+				cwd: 'css/',
+				src: ['pattern.css', '!pattern.min.css'],
+				dest: 'css/',
+				ext: '.min.css'
+			}
+		},
+		jshint: {
+			files: ['Gruntfile.js'],
+			options: {
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true,
+					document: true
+				}
+			}
+		},
+		concat: {
+			options: {
+				separator: ';'
+			},
+			js: {
+				src: ['js/classie.js', 'js/pattern.js'],
+				dest: 'js/pattern.min.js'
+			}
+		},
+		watch: {
+			files: [
+			'/*',
+			'css/*.scss',
+			'css/*.css',
+			'js/*.js',
+			],
+			tasks: 'default'
+		}
+  });
+
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+grunt.registerTask('default', ['sass', 'uglify', 'concat', 'jshint', 'cssmin']);
+grunt.registerTask('watch', ['sass', 'uglify', 'concat', 'jshint', 'cssmin']);
+};
